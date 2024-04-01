edae3802671584aea6eeaa65f02603e1a06d3e8e
Mohd Tahzeeb Khan <tahzeebk80@gmail.com>
2024-04-02 01:42:07 +0530

    <Profit Growth Table> data is fetch from the Source website

--- calculation.py
+++ calculation.py
@@ -19,3 +19,11 @@ def get_webdata(symbol):
         for data in tdd:
             data1=data.get_text().replace("%", "")
             sales_growth_list.append(data1) #-----------------------Sales Growth 
+    profit_growth=soup.find_all('table',class_="ranges-table")[1]
+    tr=profit_growth.find_all("tr")
+    for td in tr:
+        tdd=td.find_all("td")
+        tdd=tdd[1::2]
+        for data in tdd:
+            data1=data.get_text().replace("%", "")
+            profit_growth_list.append(data1) #------------------------Profit Growth
